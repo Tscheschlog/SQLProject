8e06daf3bbdb7a95e3c329651d41b6a6a9c89212
Luke <lkhepokoski2129@eagle.fgcu.edu>
2023-04-12 14:09:28 -0400

    "added SQL statements for Assignment 2"

--- components/internshipschema.sql
+++ components/internshipschema.sql
@@ -5,14 +5,6 @@
 -- DROP TABLE student;
 -- DROP TABLE tag;
 
--- DROP TABLE internship_survey;
--- DROP TABLE form;
--- DROP TABLE company;
--- DROP TABLE  internship;
--- DROP TABLE student;
--- DROP TABLE tag;
-
-
 CREATE TABLE student (
   student_id INTEGER PRIMARY KEY,
   first_name VARCHAR (20) NOT NULL,
@@ -69,3 +61,53 @@ CREATE TABLE internship_survey(
     FOREIGN KEY (tag_id) REFERENCES tag,
     UNIQUE (survey_id)
 );
+
+
+-- DML SQL Statements for Assignment 2
+
+-- Select student by username for login
+SELECT *
+FROM student
+WHERE email = 'email';
+
+-- List of prior submissions by student id
+SELECT *
+FROM internship_survey
+WHERE student_id = 'student_id';
+
+-- list of companies
+SELECT name
+FROM company;
+
+-- list of tags
+SELECT name
+FROM tag;
+
+-- Update statement to edit text entered into submitted data
+UPDATE internship_survey
+SET text_data = ('value');
+
+-- insert for new company
+INSERT INTO company (company_id, name)
+VALUES ('company_id', 'name');
+
+-- insert for new tags
+INSERT INTO tag (tag_id, name)
+VALUES ('tag_id', 'name');
+
+-- Create view for submission data
+CREATE VIEW submission_data AS
+SELECT isv.survey_id, isv.date_submitted,
+s.first_name, s.last_name, s.email,
+c.name AS company_name,
+i.title AS internship_title,
+(
+  SELECT GROUP_CONCAT(t2.name, ', ')
+  FROM internship_survey isv2
+  JOIN tag t2 ON isv2.tag_id = t2.tag_id
+  WHERE isv2.survey_id = isv.survey_id
+) AS tag_names
+FROM internship_survey isv
+JOIN company c ON isv.company_id = c.company_id
+JOIN internship i ON isv.internship_id = i.internship_id
+JOIN student s ON isv.student_id = s.student_id;
