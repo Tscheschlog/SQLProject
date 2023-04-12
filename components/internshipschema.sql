a8c78c1bbbae8e698a91760253e4031cf1945717
Luke <lkhepokoski2129@eagle.fgcu.edu>
2023-04-12 13:46:25 -0400

    "Updated SQL File with statements"

--- components/internshipschema.sql
+++ components/internshipschema.sql
@@ -5,43 +5,55 @@
 -- DROP TABLE student;
 -- DROP TABLE tag;
 
+-- DROP TABLE internship_survey;
+-- DROP TABLE form;
+-- DROP TABLE company;
+-- DROP TABLE  internship;
+-- DROP TABLE student;
+-- DROP TABLE tag;
+
 
 CREATE TABLE student (
   student_id INTEGER PRIMARY KEY,
-  first_name VARCHAR,
-  last_name VARCHAR,
-  email VARCHAR
-
+  first_name VARCHAR (20) NOT NULL,
+  last_name VARCHAR (20) NOT NULL,
+  email VARCHAR (50) NOT NULL,
+  UNIQUE (email),
+  UNIQUE (student_id)
 );
 
 CREATE TABLE internship(
     internship_id INTEGER PRIMARY KEY,
-    title VARCHAR,
+    title VARCHAR (50), NOT NULL,
     start_date VARCHAR,
     end_date VARCHAR,
     status INTEGER (1),
-    company_id VARCHAR,
-    student_id VARCHAR,
+    company_id INTEGER NOT NULL,
+    student_id INTEGER,
     FOREIGN KEY (company_id) REFERENCES company,
-    FOREIGN KEY (student_id) REFERENCES student
+    FOREIGN KEY (student_id) REFERENCES student,
+    UNIQUE (internship_id)
 );
 
 CREATE TABLE company (
     company_id INTEGER PRIMARY KEY,
-    name VARCHAR
+    name VARCHAR,
+    UNIQUE (company_id)
 
 );
 
 CREATE TABLE tag (
     tag_id INTEGER PRIMARY KEY,
-    name VARCHAR
+    name VARCHAR (30),
+    UNIQUE (tag_id)
 
 );
 
 CREATE TABLE form (
     form_id INTEGER PRIMARY KEY,
-    title VARCHAR,
-    description TEXT
+    title VARCHAR (50),
+    description TEXT,
+    UNIQUE (form_id)
 );
 
 CREATE TABLE internship_survey(
@@ -54,5 +66,6 @@ CREATE TABLE internship_survey(
     FOREIGN KEY (form_id) REFERENCES form,
     FOREIGN KEY (internship_id) REFERENCES internship,
     FOREIGN KEY (student_id) REFERENCES student,
-    FOREIGN KEY (tag_id) REFERENCES tag
+    FOREIGN KEY (tag_id) REFERENCES tag,
+    UNIQUE (survey_id)
 );
